0fecc3ac0579a31eecf5715edfb261336c68d3d3
Rezwan Mazumder <rezwanmazumder@gmail.com>
2019-05-03 01:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(Eid,Rezwan) {
   return `Happy ${Eid}, ${Rezwan}!`;
 }
 
-function holidayCountdown(Eid,five) {
-  return `It's ${five} days until ${Eid}!`
+function holidayCountdown(five,Dock) {
+  return `It's ${five} days until ${Dock}!`
 }
